5ad8a029988446fa6ada08ba7c01291e598e0ad4
TaikiYamada4 <taiki.yamada@tier4.jp>
2025-02-28 16:36:43 +0900

    implemented mapping.traffic_light.light_bulb_tagging
    Signed-off-by: TaikiYamada4 <taiki.yamada@tier4.jp>

---
+++ autoware_lanelet2_map_validator/src/include/lanelet2_map_validator/validators/traffic_light/light_bulb_tagging.hpp
@@ -0,0 +1,43 @@
+// Copyright 2025 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LANELET2_MAP_VALIDATOR__VALIDATORS__TRAFFIC_LIGHT__LIGHT_BULB_TAGGING_HPP_
+#define LANELET2_MAP_VALIDATOR__VALIDATORS__TRAFFIC_LIGHT__LIGHT_BULB_TAGGING_HPP_
+
+#include <lanelet2_validation/Validation.h>
+#include <lanelet2_validation/ValidatorFactory.h>
+
+#include <set>
+#include <string>
+
+namespace lanelet::autoware::validation
+{
+class LightBulbsTaggingValidator : public lanelet::validation::MapValidator
+{
+public:
+  // Write the validator's name here
+  constexpr static const char * name() { return "mapping.traffic_light.light_bulb_tagging"; }
+
+  lanelet::validation::Issues operator()(const lanelet::LaneletMap & map) override;
+
+private:
+  lanelet::validation::Issues check_light_bulb_tagging(const lanelet::LaneletMap & map);
+
+  inline static const std::set<std::string> expected_colors_ = {"red", "yellow", "green"};
+  inline static const std::set<std::string> expected_arrows_ = {
+    "up", "right", "left", "up_right", "up_left"};
+};
+}  // namespace lanelet::autoware::validation
+
+#endif  // LANELET2_MAP_VALIDATOR__VALIDATORS__TRAFFIC_LIGHT__LIGHT_BULB_TAGGING_HPP_
---
+++ autoware_lanelet2_map_validator/src/validators/traffic_light/light_bulb_tagging.cpp
@@ -0,0 +1,87 @@
+// Copyright 2025 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lanelet2_map_validator/validators/traffic_light/light_bulb_tagging.hpp"
+
+#include "lanelet2_map_validator/utils.hpp"
+
+#include <lanelet2_core/LaneletMap.h>
+
+#include <set>
+#include <string>
+
+namespace lanelet::autoware::validation
+{
+namespace
+{
+lanelet::validation::RegisterMapValidator<LightBulbsTaggingValidator> reg;
+}
+
+lanelet::validation::Issues LightBulbsTaggingValidator::operator()(const lanelet::LaneletMap & map)
+{
+  lanelet::validation::Issues issues;
+
+  lanelet::autoware::validation::appendIssues(issues, check_light_bulb_tagging(map));
+
+  return issues;
+}
+
+lanelet::validation::Issues LightBulbsTaggingValidator::check_light_bulb_tagging(
+  const lanelet::LaneletMap & map)
+{
+  lanelet::validation::Issues issues;
+
+  std::set<lanelet::Id> validated_bulbs;
+
+  // Search from the regulatory element layer since the linestring layer might be too huge
+  for (const auto & reg_elem : map.regulatoryElementLayer) {
+    const auto light_bulbs_linestrings =
+      reg_elem->getParameters<lanelet::ConstLineString3d>("light_bulbs");
+    for (const lanelet::ConstLineString3d & light_bulbs : light_bulbs_linestrings) {
+      if (validated_bulbs.find(light_bulbs.id()) != validated_bulbs.end()) {
+        continue;
+      }
+      validated_bulbs.insert(light_bulbs.id());
+
+      for (const lanelet::ConstPoint3d & bulb : light_bulbs) {
+        if (!bulb.hasAttribute("color")) {
+          issues.emplace_back(
+            lanelet::validation::Severity::Error, lanelet::validation::Primitive::Point, bulb.id(),
+            append_issue_code_prefix(
+              this->name(), 1, "A point representing a light bulb should have a color tag."));
+        } else if (
+          expected_colors_.find(bulb.attribute("color").value()) == expected_colors_.end()) {
+          issues.emplace_back(
+            lanelet::validation::Severity::Error, lanelet::validation::Primitive::Point, bulb.id(),
+            append_issue_code_prefix(
+              this->name(), 2,
+              "The color of a light bulb should be either red, yellow, or green."));
+        }
+
+        if (
+          bulb.hasAttribute("arrow") &&
+          expected_arrows_.find(bulb.attribute("arrow").value()) == expected_arrows_.end()) {
+          issues.emplace_back(
+            lanelet::validation::Severity::Error, lanelet::validation::Primitive::Point, bulb.id(),
+            append_issue_code_prefix(
+              this->name(), 3,
+              "The arrow of a light bulb should be either up, right, left, up_right, or up_left."));
+        }
+      }
+    }
+  }
+
+  return issues;
+}
+}  // namespace lanelet::autoware::validation
