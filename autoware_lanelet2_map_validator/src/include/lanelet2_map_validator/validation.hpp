6e81aa1335632804fc89de8149ead7f999d36d2b
Mamoru Sobue <hilo.soblin@gmail.com>
2025-06-05 13:11:18 +0900

    refactor: add docstring, comment and disambiguate std::string/ValidatorName (#47)
    Signed-off-by: Mamoru Sobue <hilo.soblin@gmail.com>
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>
    Co-authored-by: Taiki Yamada <129915538+TaikiYamada4@users.noreply.github.com>

--- autoware_lanelet2_map_validator/src/include/lanelet2_map_validator/validation.hpp
+++ autoware_lanelet2_map_validator/src/include/lanelet2_map_validator/validation.hpp
@@ -37,41 +37,78 @@ using json = nlohmann::json;
 
 namespace lanelet::autoware::validation
 {
+
+/**
+ * @brief alias for code clarity
+ */
+using ValidatorName = std::string;
+
 struct ValidatorInfo
 {
   enum class Severity { ERROR, WARNING, INFO, NONE };
 
-  std::unordered_map<std::string, bool> prereq_with_forgive_warnings;
+  std::unordered_map<ValidatorName, bool>
+    prereq_with_forgive_warnings;  //<! Validator(1st), forgive_warning(2nd)
+
   Severity max_severity = Severity::NONE;
 };
 
-using Validators = std::unordered_map<std::string, ValidatorInfo>;
+using Validators = std::unordered_map<ValidatorName, ValidatorInfo>;
 
+/**
+ * @brief 1st is either of "point", "linestring", "polygon", "lanelet", "area", "regulatory
+ * element", "primitive"
+ */
 using SimplePrimitive = std::pair<std::string, lanelet::Id>;
-using ValidatorExclusionMap = std::map<std::string, std::vector<SimplePrimitive>>;
 
+using ValidatorExclusionMap = std::map<ValidatorName, std::vector<SimplePrimitive>>;
+
+/**
+ * @brief simply call lanelet::validation::validateMap
+ * @return return lanelet::validation::validateMap()
+ */
 std::vector<lanelet::validation::DetectedIssues> apply_validation(
   const lanelet::LaneletMap & lanelet_map,
   const lanelet::validation::ValidationConfig & val_config);
 
 Validators parse_validators(const json & json_data);
 
-std::tuple<std::queue<std::string>, Validators> create_validation_queue(
+/**
+ * @brief topologically sort the validators
+ * @return names of validators(1st), remaining validators(2nd, usually normal)
+ */
+std::tuple<std::queue<ValidatorName>, Validators> create_validation_queue(
   const Validators & validators);
 
-// Function to find a validator block by name
-json & find_validator_block(json & json_data, const std::string & validator_name);
+/**
+ * @brief find a validator block by name
+ */
+json & find_validator_block(json & json_data, const ValidatorName & validator_name);
 
+/**
+ * @brief set error to json and output it for invalid prerequisite setting
+ */
 std::vector<lanelet::validation::DetectedIssues> describe_unused_validators_to_json(
   json & json_data, const Validators & unused_validators);
 
+/**
+ * @brief if the prerequisites are SEVERE or non-ignorable WARNING, report that "Prerequisites
+ * didn't pass"
+ */
 std::vector<lanelet::validation::DetectedIssues> check_prerequisite_completion(
-  const Validators & validators, const std::string target_validator_name);
+  const Validators & validators, const ValidatorName & target_validator_name);
 
+/**
+ * @brief check if requirement have passed, count the number of error/warning, etc., then set it to
+ * json_data and print to stdout
+ */
 void summarize_validator_results(json & json_data);
 
+/**
+ * @brief helper to create a ValidationConfig with .checksFilter = {validator_name}
+ */
 lanelet::validation::ValidationConfig replace_validator(
-  const lanelet::validation::ValidationConfig & input, const std::string & validator_name);
+  const lanelet::validation::ValidationConfig & input, const ValidatorName & validator_name);
 
 std::vector<lanelet::validation::DetectedIssues> validate_all_requirements(
   json & json_data, const lanelet::autoware::validation::MetaConfig & validator_config,
@@ -81,6 +118,10 @@ void export_results(json & json_data, const std::string output_file_path);
 
 ValidatorExclusionMap import_exclusion_list(const json & json_data);
 
+/**
+ * @brief this functions is used by giving ValidationExclusionMap[validator_foo] as the second
+ * argument and filter out the 1st argument
+ */
 void filter_out_primitives(
   std::vector<lanelet::validation::DetectedIssues> & issues_vector,
   std::vector<SimplePrimitive> primitive_list);
