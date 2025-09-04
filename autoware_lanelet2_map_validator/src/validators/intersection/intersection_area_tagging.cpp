4069972bd8f68f1acb5c24e71ad027b06f36fa16
mraditya01 <mraditya01@yahoo.com>
2025-09-05 01:11:13 +0900

    initial commit

---
+++ autoware_lanelet2_map_validator/src/validators/intersection/intersection_area_tagging.cpp
@@ -0,0 +1,110 @@
+// Copyright 2025 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lanelet2_map_validator/validators/intersection/intersection_area_tagging.hpp"
+
+#include "lanelet2_map_validator/utils.hpp"
+
+#include <boost/geometry/algorithms/within.hpp>
+
+#include <lanelet2_core/LaneletMap.h>
+#include <lanelet2_core/geometry/Polygon.h>
+#include <lanelet2_core/primitives/Polygon.h>
+
+#include <map>
+#include <string>
+#include <unordered_set>
+
+namespace lanelet::autoware::validation
+{
+namespace
+{
+lanelet::validation::RegisterMapValidator<IntersectionAreaTaggingValidator> reg;
+}
+
+lanelet::validation::Issues IntersectionAreaTaggingValidator::operator()(const lanelet::LaneletMap & map)
+{
+  lanelet::validation::Issues issues;
+
+  lanelet::autoware::validation::appendIssues(issues, check_intersection_area_tagging(map));
+
+  return issues;
+}
+
+lanelet::validation::Issues IntersectionAreaTaggingValidator::check_intersection_area_tagging(const lanelet::LaneletMap & map)
+{
+  lanelet::validation::Issues issues;
+
+  std::map<lanelet::Id, lanelet::ConstPolygon3d> intersection_areas;
+  for (const lanelet::ConstPolygon3d & polygon3d : map.polygonLayer) {
+    if (
+      polygon3d.hasAttribute(lanelet::AttributeName::Type) &&
+      polygon3d.attribute(lanelet::AttributeName::Type).value() == "intersection_area") {
+      intersection_areas[polygon3d.id()] = polygon3d;
+    }
+  }
+
+  for (const auto & [area_id, area_polygon] : intersection_areas) {
+    lanelet::BasicPolygon2d area_polygon2d = lanelet::traits::to2D(area_polygon.basicPolygon());
+    
+    for (const lanelet::ConstLanelet & lanelet : map.laneletLayer) {
+      lanelet::BasicPolygon2d lanelet_polygon = lanelet.polygon2d().basicPolygon();
+      if (boost::geometry::within(lanelet_polygon, area_polygon2d)) {
+        std::string intersection_area_attr = lanelet.attributeOr("intersection_area", "none");
+        if (intersection_area_attr == "none") {
+          // Issue-001: Lanelet missing intersection_area tag
+          std::map<std::string, std::string> area_id_map;
+          area_id_map["area_id"] = std::to_string(area_id);
+          issues.emplace_back(
+            construct_issue_from_code(issue_code(this->name(), 1), lanelet.id(), area_id_map));
+        } else {
+          lanelet::Id tagged_area_id = static_cast<lanelet::Id>(std::atoi(intersection_area_attr.c_str()));
+          if (tagged_area_id != area_id) {
+            // Issue-002: Lanelet has wrong intersection_area tag
+            std::map<std::string, std::string> tag_map;
+            tag_map["expected_area_id"] = std::to_string(area_id);
+            tag_map["actual_area_id"] = intersection_area_attr;
+            issues.emplace_back(
+              construct_issue_from_code(issue_code(this->name(), 2), lanelet.id(), tag_map));
+          }
+        }
+      }
+    }
+  }
+
+  // Issue-003: Lanelet has intersection_area tag but is not completely covered by the referenced area
+  for (const lanelet::ConstLanelet & lanelet : map.laneletLayer) {
+    std::string intersection_area_attr = lanelet.attributeOr("intersection_area", "none");
+    if (intersection_area_attr != "none") {
+      lanelet::Id tagged_area_id = static_cast<lanelet::Id>(std::atoi(intersection_area_attr.c_str()));
+      
+      auto area_it = intersection_areas.find(tagged_area_id);
+      if (area_it == intersection_areas.end()) {
+        continue;
+      }
+
+      lanelet::BasicPolygon2d area_polygon2d = lanelet::traits::to2D(area_it->second.basicPolygon());
+      lanelet::BasicPolygon2d lanelet_polygon = lanelet.polygon2d().basicPolygon();
+      if (!boost::geometry::within(lanelet_polygon, area_polygon2d)) {
+        std::map<std::string, std::string> area_id_map;
+        area_id_map["area_id"] = std::to_string(tagged_area_id);
+        issues.emplace_back(
+          construct_issue_from_code(issue_code(this->name(), 3), lanelet.id(), area_id_map));
+      }
+    }
+  }
+
+  return issues;
+}
+}  // namespace lanelet::autoware::validation
---
+++ autoware_lanelet2_map_validator/test/src/intersection/test_intersection_area_tagging.cpp
@@ -0,0 +1,127 @@
+// Copyright 2025 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "map_validation_tester.hpp"
+#include "lanelet2_map_validator/validators/intersection/intersection_area_tagging.hpp"
+
+#include <gtest/gtest.h>
+#include <lanelet2_core/LaneletMap.h>
+
+#include <string>
+
+class TestIntersectionAreaTaggingValidator : public MapValidationTester
+{
+protected:
+  const std::string test_target_ = 
+    std::string(lanelet::autoware::validation::IntersectionAreaTaggingValidator::name());
+};
+
+TEST_F(TestIntersectionAreaTaggingValidator, ValidatorAvailability)  // NOLINT for gtest
+{
+  std::string expected_validator_name = lanelet::autoware::validation::IntersectionAreaTaggingValidator::name();
+
+  lanelet::validation::Strings validators =
+    lanelet::validation::availabeChecks(expected_validator_name);  // cspell:disable-line
+
+  const uint32_t expected_validator_num = 1;
+  EXPECT_EQ(expected_validator_num, validators.size());
+  EXPECT_EQ(expected_validator_name, validators[0]);
+}
+
+TEST_F(TestIntersectionAreaTaggingValidator, SampleMap)  // NOLINT for gtest
+{
+  load_target_map("sample_map.osm");
+
+  lanelet::autoware::validation::IntersectionAreaTaggingValidator checker;
+  const auto & issues = checker(*map_);
+
+  EXPECT_EQ(issues.size(), 0);
+}
+
+TEST_F(TestIntersectionAreaTaggingValidator, MissingIntersectionAreaTag)  // NOLINT for gtest
+{
+  load_target_map("intersection/intersection_area_without_intersection_area_tag.osm");
+
+  lanelet::autoware::validation::IntersectionAreaTaggingValidator checker;
+  const auto & issues = checker(*map_);
+
+  EXPECT_EQ(issues.size(), 1);
+
+  const lanelet::Id expected_lanelet_id = 49;
+  const lanelet::Id expected_area_id = 10803;
+  
+  std::map<std::string, std::string> area_id_map;
+  area_id_map["area_id"] = std::to_string(expected_area_id);
+  const auto expected_issue = 
+    construct_issue_from_code(issue_code(test_target_, 1), expected_lanelet_id, area_id_map);
+
+  const auto difference = compare_an_issue(expected_issue, issues[0]);
+  EXPECT_TRUE(difference.empty()) << difference;
+  EXPECT_EQ(issues[0].id, expected_lanelet_id);
+}
+
+TEST_F(TestIntersectionAreaTaggingValidator, IncorrectIntersectionAreaTag)  // NOLINT for gtest
+{
+  load_target_map("intersection/intersection_area_with_wrong_intersection_area_id.osm");
+
+  lanelet::autoware::validation::IntersectionAreaTaggingValidator checker;
+  const auto & issues = checker(*map_);
+
+  EXPECT_EQ(issues.size(), 1);
+
+  const lanelet::Id expected_lanelet_id = 49;
+  const lanelet::Id expected_area_id = 10803;
+  const lanelet::Id wrong_area_id = 2195;
+  
+  std::map<std::string, std::string> tag_map;
+  tag_map["expected_area_id"] = std::to_string(expected_area_id);
+  tag_map["actual_area_id"] = std::to_string(wrong_area_id);
+  const auto expected_issue = 
+    construct_issue_from_code(issue_code(test_target_, 2), expected_lanelet_id, tag_map);
+
+  const auto difference = compare_an_issue(expected_issue, issues[0]);
+  EXPECT_TRUE(difference.empty()) << difference;
+  EXPECT_EQ(issues[0].id, expected_lanelet_id);
+}
+
+TEST_F(TestIntersectionAreaTaggingValidator, LaneletNotCoveredByIntersectionArea)  // NOLINT for gtest
+{
+  load_target_map("intersection/intersection_area_with_lanelet_not_covered_by_intersection_area.osm");
+
+  lanelet::autoware::validation::IntersectionAreaTaggingValidator checker;
+  const auto & issues = checker(*map_);
+
+  EXPECT_EQ(issues.size(), 3);
+
+  const lanelet::Id expected_lanelet_ids[3] = {52, 53, 54};
+  const lanelet::Id expected_area_id = 10803;
+  
+  for (int i = 0; i < 3; i++) {
+    std::map<std::string, std::string> area_id_map;
+    area_id_map["area_id"] = std::to_string(expected_area_id);
+    const auto expected_issue = 
+      construct_issue_from_code(issue_code(test_target_, 3), expected_lanelet_ids[i], area_id_map);
+
+    bool found = false;
+    for (const auto & issue : issues) {
+      if (issue.id == expected_lanelet_ids[i]) {
+        const auto difference = compare_an_issue(expected_issue, issue);
+        EXPECT_TRUE(difference.empty()) << difference;
+        found = true;
+        break;
+      }
+    }
+    EXPECT_TRUE(found) << "No issue found for lanelet ID " << expected_lanelet_ids[i];
+  }
+}
