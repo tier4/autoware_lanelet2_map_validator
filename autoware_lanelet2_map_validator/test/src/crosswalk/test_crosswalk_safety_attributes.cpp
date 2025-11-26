4a83fee9e601037a0f9eba73886380fd83ef6b51
Giovanni Muhammad Raditya <mraditya01@yahoo.com>
2025-11-26 12:00:06 +0900

    feat(crosswalk): added validator for crosswalk safety attributes (vm-05-03) (#124)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ autoware_lanelet2_map_validator/test/src/crosswalk/test_crosswalk_safety_attributes.cpp
@@ -0,0 +1,111 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lanelet2_map_validator/validators/crosswalk/crosswalk_safety_attributes.hpp"
+#include "map_validation_tester.hpp"
+
+#include <gtest/gtest.h>
+#include <lanelet2_core/LaneletMap.h>
+
+#include <map>
+#include <string>
+
+class TestCrosswalkSafetyAttributesValidator : public MapValidationTester
+{
+protected:
+  const std::string test_target_ =
+    std::string(lanelet::autoware::validation::CrosswalkSafetyAttributesValidator::name());
+};
+
+TEST_F(TestCrosswalkSafetyAttributesValidator, ValidatorAvailability)  // NOLINT for gtest
+{
+  std::string expected_validator_name =
+    lanelet::autoware::validation::CrosswalkSafetyAttributesValidator::name();
+
+  lanelet::validation::Strings validators =
+    lanelet::validation::availabeChecks(expected_validator_name);  // cspell:disable-line
+
+  const uint32_t expected_validator_num = 1;
+  EXPECT_EQ(expected_validator_num, validators.size());
+  EXPECT_EQ(expected_validator_name, validators[0]);
+}
+
+TEST_F(TestCrosswalkSafetyAttributesValidator, SampleMap)  // NOLINT for gtest
+{
+  load_target_map("sample_map.osm");
+
+  lanelet::autoware::validation::CrosswalkSafetyAttributesValidator checker;
+  const auto & issues = checker(*map_);
+
+  EXPECT_EQ(issues.size(), 0);
+}
+
+TEST_F(TestCrosswalkSafetyAttributesValidator, NegativeSlowDownSpeed)  // NOLINT for gtest
+{
+  load_target_map("crosswalk/crosswalk_with_negative_slow_down_speed.osm");
+
+  lanelet::autoware::validation::CrosswalkSafetyAttributesValidator checker;
+  const auto & issues = checker(*map_);
+
+  std::map<std::string, std::string> reason_map;
+  reason_map["attribute_value"] = "-5";
+  const auto expected_issue =
+    construct_issue_from_code(issue_code(test_target_, 1), 18, reason_map);
+
+  EXPECT_EQ(issues.size(), 1);
+
+  const auto difference = compare_an_issue(expected_issue, issues[0]);
+  EXPECT_TRUE(difference.empty()) << difference;
+}
+
+TEST_F(TestCrosswalkSafetyAttributesValidator, NegativeSlowDownDistance)  // NOLINT for gtest
+{
+  load_target_map("crosswalk/crosswalk_with_negative_slow_down_distance.osm");
+
+  lanelet::autoware::validation::CrosswalkSafetyAttributesValidator checker;
+  const auto & issues = checker(*map_);
+
+  std::map<std::string, std::string> reason_map;
+  reason_map["attribute_value"] = "-10";
+  const auto expected_issue =
+    construct_issue_from_code(issue_code(test_target_, 2), 18, reason_map);
+
+  EXPECT_EQ(issues.size(), 1);
+
+  const auto difference = compare_an_issue(expected_issue, issues[0]);
+  EXPECT_TRUE(difference.empty()) << difference;
+}
+
+TEST_F(TestCrosswalkSafetyAttributesValidator, NonNumericalValue)  // NOLINT for gtest
+{
+  load_target_map("crosswalk/crosswalk_with_non_numerical_distance_speed.osm");
+
+  lanelet::autoware::validation::CrosswalkSafetyAttributesValidator checker;
+  const auto & issues = checker(*map_);
+
+  std::map<std::string, std::string> reason_map_speed;
+  reason_map_speed["attribute_value"] = "5 km/h";
+  const auto expected_issue_speed =
+    construct_issue_from_code(issue_code(test_target_, 1), 18, reason_map_speed);
+
+  std::map<std::string, std::string> reason_map_distance;
+  reason_map_distance["attribute_value"] = "10 km";
+  const auto expected_issue_distance =
+    construct_issue_from_code(issue_code(test_target_, 2), 18, reason_map_distance);
+
+  const auto expected_issues = {expected_issue_speed, expected_issue_distance};
+
+  const auto difference = compare_issues(expected_issues, issues);
+  EXPECT_TRUE(difference.empty()) << difference;
+}
